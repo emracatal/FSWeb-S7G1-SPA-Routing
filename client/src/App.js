45955afabf061775fe634378f04d82c0d4e8f014
emracatal <134175723+emracatal@users.noreply.github.com>
2023-08-08 01:26:00 +0300

    ilk commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter, Switch, Route } from "react-router-dom/";
+import KaydedilenlerListesi from "./Filmler/KaydedilenlerListesi";
+import Film from "./Filmler/Film";
+import FilmListesi from "./Filmler/FilmListesi";
 
-import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
-
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const FilmleriAl = () => {
       axios
-        .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
-        .then(response => {
+        .get("http://localhost:5001/api/filmler") // Burayı Postman'le çalışın??
+        .then((response) => {
+          console.log(response.data);
+          setMovieList(response.data);
           // Bu kısmı log statementlarıyla çalışın
           // ve burdan gelen response'u 'movieList' e aktarın
         })
-        .catch(error => {
-          console.error('Sunucu Hatası', error);
+        .catch((error) => {
+          console.log("Sunucu Hatası", error);
         });
-    }
+    };
     FilmleriAl();
   }, []);
 
-  const KaydedilenlerListesineEkle = id => {
+  const KaydedilenlerListesineEkle = (id) => {
     // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
   };
 
   return (
-    <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
+    <BrowserRouter>
+      <div>
+        {loading && <p>loading..</p>}
+        {error && <p>{error}</p>}
+        {!loading && !error && <h1>Kaydedilenler Listesi</h1>}
+        <KaydedilenlerListesi
+          list={
+            [
+              /* Burası esnek */
+            ]
+          }
+        />
 
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
-    </div>
+        <div>
+          <Switch>
+            <Route path="/film" exact>
+              <Film />
+            </Route>
+
+            <Route path="/filmListesi" exact>
+              <FilmListesi movies={movieList} />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
+
+// import { BrowserRouter } from "react-router-dom/" ekledim, return içindekileri <BrowserRouter></BrowserRouter> ile sardım.loading ve error için useState'leri yazıp response.data'yı console'da görüntüledim.Film ve Filmlistesini import edip switch içerisine routerlerini ekledim.
